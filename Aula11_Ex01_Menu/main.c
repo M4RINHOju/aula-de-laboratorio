ce53ebc5d21fbe812dd8f98e86a2da121d147f2b
M4RINHOju <111473217+M4RINHOju@users.noreply.github.com>
2022-10-19 20:02:34 -0300

    Add files via upload

---
+++ Aula11_Ex01_Menu/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int opc = 0;
+
+	while (opc != 9) {
+		printf(" \n");
+		printf(" \n");
+		printf(" \n");
+		printf(" \n");
+		printf("9. Sair \n");
+		scanf("%i", &opc);
+		
+		if (opc == 1) {
+			//sub-menu
+			
+		} else if (opc == 2) {
+			
+		} else if (opc == 3) {
+			
+		} else if (opc == 4) {
+			
+		}		
+	}	
+	
+	system("pause");
+	return 0;
+}
